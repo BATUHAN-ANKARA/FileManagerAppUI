445f7bf0cb99d8b1bba205234e657e40a1907b7a
Batuhan-ankara <sertgumecbatuhan@gmail.com>
2022-12-20 01:14:25 +0300

    20.12 Commits

--- src/screens/Storage.js
+++ src/screens/Storage.js
@@ -1,12 +1,44 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet, Image} from 'react-native'
 
-const Storage = () => {
+const Storage = ({image, title}) => {
   return (
-    <View>
-      <Text>Storage Screen</Text>
+    <View style={styles.container}>
+      <Image source={image} style={styles.img} />
+      <View style={{paddingLeft: 20}}>
+        <Text>{title}</Text>
+        <Text style={styles.day}>17 April 2017</Text>
+        <Text style={styles.desc}>Files: 75 items: 620 Used:60GB</Text>
+      </View>
     </View>
   )
 }
 
 export default Storage
+
+const styles = StyleSheet.create({
+  container: {
+    width: 320,
+    height: 80,
+    flex: 1,
+    backgroundColor: '#f4f5f3',
+    alignSelf: 'center',
+    borderRadius: 20,
+    marginTop: 10,
+    padding: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  img: {
+    width: 50,
+    height: 50,
+    resizeMode: 'center',
+  },
+  day: {
+    fontSize: 12,
+    color: '#d8d8d8',
+  },
+  desc: {
+    fontSize: 11,
+  },
+})
